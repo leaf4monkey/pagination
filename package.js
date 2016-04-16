d16bf1f0e11a44a1918b782bb4317262a8c8b684
Chen Wen <joc98@163.com>
2016-04-17 00:49:25 +0800

    增加描述和git地址
    Signed-off-by: Chen Wen <joc98@163.com>

--- package.js
+++ package.js
@@ -1,10 +1,10 @@
 Package.describe({
     name: 'leaf4monkey:pagination',
-    version: '0.0.1',
+    version: '0.0.2',
     // Brief, one-line summary of the package.
-    summary: '',
+    summary: 'a light weight pagination package for meteor.',
     // URL to the Git repository containing the source code for this package.
-    git: '',
+    git: 'https://github.com/leaf4monkey/pagination.git',
     // By default, Meteor will default to using README.md for documentation.
     // To avoid submitting documentation, set this field to null.
     documentation: 'README.md'
